4d759ada003c899b5eea675937df9ff91496f489
roberto_hinojosa <roberto_hinojosa@hotmail.com>
2020-10-29 22:27:56 -0600

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import Adapter from "enzyme-adapter-react-16";
+import Enzyme, { shallow } from "enzyme";
+import { MY_DATA } from "./data";
+Enzyme.configure({ adapter: new Adapter() });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe(App, () => {
+  const app = shallow(<App />);
+
+  it("renders correctly", () => {
+    expect(app).toMatchSnapshot();
+  });
+  it("initializes the `state` with initial data", () => {
+    expect(app.state().data).toEqual(MY_DATA);
+  });
+  
---
+++ src/data.jsx
@@ -0,0 +1,9 @@
+export const MY_DATA = [
+    { id: 1, name: "Juan Carlos ",lastName:"Gonzalez",materno:"Perez", rfc: "GC12345889" },
+    { id: 2, name: "Miguel de Leon",lastName:"de Leon",materno:"Gomez", rfc: "MMDh3874994" },
+    { id: 3, name: "Lorenzo ",lastName:"Juarez",materno:"Fer",  rfc: "LoMurt12313" },
+    { id: 4, name: "John Perez",lastName:"Perez",materno:"Gonzaga", rfc: "JP1535465" },
+    { id: 5, name: "Armando ",lastName:"Gonzalez",materno:"Gtz", rfc: "1213246"},
+    { id: 6, name: "Ernesto",lastName:"Michelen",materno:"Piedra", rfc: "EPXXXX1321" },
+  ];
+  
